a75c934ca934d59bd830d8d9210ae85e64d3ff0b
ChaehuiSeo <sch0991@naver.com>
2025-04-07 00:10:07 +0900

    [CHORE] Improve sample apps

--- SampleApp-SwiftUI/SampleApp-SwiftUI/ContentView.swift
+++ SampleApp-SwiftUI/SampleApp-SwiftUI/ContentView.swift
@@ -9,25 +9,17 @@ import SwiftUI
 import JumpWhileLoad
 
 struct ContentView: View {
-    @State private var isGamePresented = false
-    
     var body: some View {
         ZStack{
-            Color.red
             VStack {
-                Button("Start") {
+                Button("START") {
+                    JumpWhileLoadSwiftUI.present()
                     DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
                         JumpWhileLoadSwiftUI.finishLoading()
                     }
-                    isGamePresented = true
-                    JumpWhileLoadSwiftUI.present()
                 }
                 .buttonStyle(.borderedProminent)
-                .tint(.blue) // 버튼 배경색 설정
-//                .fullScreenCover(isPresented: $isGamePresented) {
-//                    JumpWhileLoadSwiftUI.fullScreenView()
-//                }
-                
+                .tint(.blue)
             }
             .padding()
         }
